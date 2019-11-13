2ac3b9037815aa0c42106b6b8d9a2221b734af41
Juan Boniowski <jboniowski@lemonadesoftware.ca>
2019-11-13 00:00:03 -0500

    ep1 video login

---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+            string msg = string.Format("Your username is '{0}' and your password is '{1}'", txtUserName.Text, txtPassword.Text); 
+            DisplayAlert("Demo App", msg, "Ok"); 
+        }
+    }
+}
